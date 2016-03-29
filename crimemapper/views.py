320ab051a2440e32b4cc92849a3bc6dd5f108084
Michael Sullivan <sapientia.veritas@gmail.com>
2016-03-29 11:42:13 -0700

    cleaned up test code

--- crimemapper/views.py
+++ crimemapper/views.py
@@ -12,26 +12,16 @@ from crimemapper.models import (
 @view_config(route_name='map', renderer='templates/map.jinja2')
 def map_view(request):
     """Render map view on page."""
-    # import pdb; pdb.set_trace()
     lat = DBSession().query(Entry.latitude).all()
-    # print(lat)
     lng = DBSession().query(Entry.longitude).all()
-    # print(lng)
-    # place = {'lat': 47.696815491, 'lng': -122.327774048}, {'lat': 47.606815491, 'lng': -122.327774048}
-    # places = {'places': place}
-
     places = []
-    print lat # list of tuples
     for i, l in enumerate(lat):
         if i == 0:
             continue
         place = {'lat': lat[i][0], 'lng': lng[i][0]}
-        print place
-        print places
         places.append(place)
     return {'places': places}
-    # return places
-    # return {}
+
 
 
 @view_config(route_name='codes', renderer='templates/reference.jinja2')
