2a29b75d4ed8a6df3137a9979569dcd28e10d6c6
SELENA FLANNERY <SELENA.FLANNERY@GMAIL.COM>
2016-03-29 15:41:24 -0700

    #76 added dictionary categorizing all crimes

---
+++ crimemapper/crimedict.py
@@ -0,0 +1,29 @@
+CRIME_DICT = {
+    "Assault": ["Assault"],
+    "Theft": [
+        "Bike Theft", "Burglary", "Car Prowl", "Mail Theft", "Pickpocket",
+        "Purse Snatch", "Shoplifting", "Theft of Service", "Vehicle Theft",
+        "Burglary of Secure Parking Garage"
+    ],
+    "Sin": ["Gamble", "Pornography", "Prostitution"],
+    "Drugs": ["DUI", "Liqour Violations", "Narcotics",],
+    "Fraud": ["Counterfeit", "Embezzle", "Forgery", "Fraud"],
+    "Homicide": ["Homicide"],
+    "Property": [
+        "Illegal Dumping", "Lost Property", "Other Property",
+        "Property Damage", "Reckless Burning", "Recovered Property",
+        "Stolen Property",
+    ],
+    "Robbery": ["Robbery"],
+    "Weapons": ["Weapons"],
+    "Disturbances": [
+        "Disorderly Conduct", "Dispute", "Firework", "Public Nuisance",
+        "Threats", "Trespass"
+    ],
+    "Other": [
+        "Animal", "Bias Incident", "Eluding", "Escape", "Extortion",
+        "Harbor Calls", "Injury", "Loitering", "Metro", "Obstruct",
+        "Stay out of area", "Traffic", "Warrant Arrest",
+        "Violation of Court Order"
+    ],
+}
