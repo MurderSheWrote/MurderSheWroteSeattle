501f406ef63fa8021d78006dc3644e066b64ec30
GingerQ <qiangwenjing@gmail.com>
2016-03-29 16:06:50 -0700

    Revert "#95 filter html on page"
    This reverts commit 510da94b85e4322dec616b8b4a0c53df87a2f70a.

--- crimemapper/api.py
+++ crimemapper/api.py
@@ -23,7 +23,7 @@ def clean_dict(listing_collection):
     for crime in listing_collection:
         for key in crime: 
          if crime[key] == 'X':
-            crime[key] == 'NULL'
+            crime[key] = 0
     return listing_collection
 
 def make_dict(json):
