4a435cd9951e923ebf13a7229dcef10c0472b828
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-03-30 06:22:41 -0700

    checks working.   Need to get full list of crimes encorperated per check.  Need to turn off all checks when all is clicked.

--- crimemapper/crimedict.py
+++ crimemapper/crimedict.py
@@ -1,5 +1,5 @@
 CRIME_DICT = {
-    "Assault": ["Assault"],
+    "Assault": ["Assault"], # Done
     "Theft": [
         "Bike Theft", "Burglary", "Car Prowl", "Mail Theft", "Pickpocket",
         "Purse Snatch", "Shoplifting", "Theft of Service", "Vehicle Theft",
@@ -8,14 +8,43 @@ CRIME_DICT = {
     "Sin": ["Gamble", "Pornography", "Prostitution"],
     "Drugs": ["DUI", "Liqour Violations", "Narcotics",],
     "Fraud": ["Counterfeit", "Embezzle", "Forgery", "Fraud"],
-    "Homicide": ["Homicide"],
+    "Homicide": ["Homicide"], # Done
     "Property": [
         "Illegal Dumping", "Lost Property", "Other Property",
         "Property Damage", "Reckless Burning", "Recovered Property",
         "Stolen Property",
     ],
-    "Robbery": ["Robbery"],
-    "Weapons": ["Weapons"],
+    "Robbery": ["Robbery"], #Done
+    "Weapons": ["Weapons"], #Done
+    "Disturbances": [
+        "Disorderly Conduct", "Dispute", "Firework", "Public Nuisance",
+        "Threats", "Trespass"
+    ],
+    "Other": [
+        "Animal", "Bias Incident", "Eluding", "Escape", "Extortion",
+        "Harbor Calls", "Injury", "Loitering", "Metro", "Obstruct",
+        "Stay out of area", "Traffic", "Warrant Arrest",
+        "Violation of Court Order"
+    ],
+}
+
+Nadia = {
+    "Theft": [
+        "Bike Theft", "Burglary", "Car Prowl", "Mail Theft", "Pickpocket",
+        "Purse Snatch", "Shoplifting", "Theft of Service", "Vehicle Theft",
+        "Burglary of Secure Parking Garage"
+    ],
+    "Sin": ["Gamble", "Pornography", "Prostitution"],
+    "Drugs": ["DUI", "Liqour Violations", "Narcotics",],
+    "Fraud": ["Counterfeit", "Embezzle", "Forgery", "Fraud"],
+    "Homicide": ["Homicide"], # Done
+    "Property": [
+        "Illegal Dumping", "Lost Property", "Other Property",
+        "Property Damage", "Reckless Burning", "Recovered Property",
+        "Stolen Property",
+    ],
+    "Robbery": ["Robbery"], #Done
+    "Weapons": ["Weapons"], #Done
     "Disturbances": [
         "Disorderly Conduct", "Dispute", "Firework", "Public Nuisance",
         "Threats", "Trespass"
