41b9d6457b543d188cbcfc2c1b01076cea059203
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-03-30 07:31:48 -0700

    Cahnge some list for filters.

--- crimemapper/crimedict.py
+++ crimemapper/crimedict.py
@@ -1,58 +1,30 @@
 CRIME_DICT = {
-    "Assault": ["Assault"], # Done
+    "Assault": ["Assault"],
     "Theft": [
         "Bike Theft", "Burglary", "Car Prowl", "Mail Theft", "Pickpocket",
         "Purse Snatch", "Shoplifting", "Theft of Service", "Vehicle Theft",
-        "Burglary of Secure Parking Garage"
+        "BURGLARY-SECURE PARKING-RES"
     ],
     "Sin": ["Gamble", "Pornography", "Prostitution"],
-    "Drugs": ["DUI", "Liqour Violations", "Narcotics",],
+    "Drugs": [
+        "DUI", "Liquor Violations", "Narcotics", "Stay out of Area of Drugs"
+    ],
     "Fraud": ["Counterfeit", "Embezzle", "Forgery", "Fraud"],
-    "Homicide": ["Homicide"], # Done
+    "Homicide": ["Homicide"],
     "Property": [
         "Illegal Dumping", "Lost Property", "Other Property",
         "Property Damage", "Reckless Burning", "Recovered Property",
         "Stolen Property",
     ],
-    "Robbery": ["Robbery"], #Done
-    "Weapons": ["Weapons"], #Done
+    "Robbery": ["Robbery"],
+    "Weapons": ["Weapons"],
     "Disturbances": [
         "Disorderly Conduct", "Dispute", "Firework", "Public Nuisance",
         "Threats", "Trespass"
     ],
     "Other": [
-        "Animal", "Bias Incident", "Eluding", "Escape", "Extortion",
-        "Harbor Calls", "Injury", "Loitering", "Metro", "Obstruct",
-        "Stay out of area", "Traffic", "Warrant Arrest",
-        "Violation of Court Order"
-    ],
-}
-
-Nadia = {
-    "Theft": [
-        "Bike Theft", "Burglary", "Car Prowl", "Mail Theft", "Pickpocket",
-        "Purse Snatch", "Shoplifting", "Theft of Service", "Vehicle Theft",
-        "Burglary of Secure Parking Garage"
-    ],
-    "Sin": ["Gamble", "Pornography", "Prostitution"],
-    "Drugs": ["DUI", "Liqour Violations", "Narcotics",],
-    "Fraud": ["Counterfeit", "Embezzle", "Forgery", "Fraud"],
-    "Homicide": ["Homicide"], # Done
-    "Property": [
-        "Illegal Dumping", "Lost Property", "Other Property",
-        "Property Damage", "Reckless Burning", "Recovered Property",
-        "Stolen Property",
-    ],
-    "Robbery": ["Robbery"], #Done
-    "Weapons": ["Weapons"], #Done
-    "Disturbances": [
-        "Disorderly Conduct", "Dispute", "Firework", "Public Nuisance",
-        "Threats", "Trespass"
-    ],
-    "Other": [
-        "Animal", "Bias Incident", "Eluding", "Escape", "Extortion",
-        "Harbor Calls", "Injury", "Loitering", "Metro", "Obstruct",
-        "Stay out of area", "Traffic", "Warrant Arrest",
-        "Violation of Court Order"
+        "Animal Complaint", "Bias Incident", "Eluding", "Escape", "Extortion",
+        "Harbor Calls", "Injury", "Loitering", "Metro", "Obstruct", "Traffic", 
+        "Warrant Arrest", "Violation of Court Order"
     ],
 }
